d505985975c016821f8f14d6155e618123154ab2
Mahbub-Sajon <sajon.mahbub@gmail.com>
2022-03-12 17:06:21 +0600

    first commit

--- js/app.js
+++ js/app.js
@@ -1,6 +1,16 @@
 const API_KEY = `6e51236ae33535215fc01c02876566b5`;
 const searchTemperature = () =>{
     const city = document.getElementById('city-name').value;
-    const url = `https://api.openweathermap.org/data/2.5/weather?lat={lat}&lon=${city}&appid=${API_KEY}`;
-    console.log(url);
+    const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${API_KEY}`;
+    // console.log(url);
+    fetch(url)
+    .then(res => res.json())
+    .then(data => displayTemerature(data))
+}
+const setInnerText = (id, text) => {
+    document.getElementById(id).innerText = text;
+}
+const displayTemerature = temperature => {
+    setInnerText("city", temperature.name);
+    console.log(temperature);
 }
